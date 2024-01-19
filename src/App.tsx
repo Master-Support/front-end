5ab6c79ae60a38eb439bfa636bec3cf7827781f4
Logan <logan.cc@outlook.com>
2024-01-19 13:05:46 -0300

    fix: ajuste no container

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <div className="container">
+      <div className="none:container">
         <Outlet />
       </div>
     </div>
